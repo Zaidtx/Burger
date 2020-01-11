0b357e55d61356a4ae11208841558b5d991cc14b
Zaid Alhir <zaidalhir@yahoo.com>
2020-01-11 14:30:12 -0600

    Cleaned up folder structure

--- Burger/db/seeds.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO burger (burgerName) VALUES ('Tx burger');
-INSERT INTO burger (burgerName) VALUES ('bur2');
-INSERT INTO burger (burgerName, devoured) VALUES ('bur3', true);
-INSERT INTO burger (burgerName, devoured) VALUES ('bur4', true);
-INSERT INTO burger (burgerName, devoured) VALUES ('bur5', true);
-INSERT INTO burger (burgerName) VALUES ('bur6');
